d4e1e9ccee7029a6ae032cf94096aba1878c7df1
Jenya0092 <56063125+Jenya0092@users.noreply.github.com>
2020-10-26 14:57:42 +0200

    Add files via upload

---
+++ 2.3.19 (4).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int pow;
+    long num =1;
+    double m_num =1;
+    cout<<"Enter pow which you want to apply to 2 (it has to be between -13 and 63) : ";
+    cin>>pow;
+    if(pow<=63 && pow>=1){
+        int i;
+        for (i=1; i<=pow; i++){
+            num=num*2;
+        }
+        cout<<num;
+    }
+       if(pow>=-20 && pow<=0){
+        int h;
+        for (h=1; h<=pow*(-1); h++){
+            num=num*2;
+            m_num = 1./num;
+            
+            
+        }
+        
+        cout<<setprecision(15)<<m_num;
+        
+    }
+    
+  
+}
---
+++ 2.3.19 (6).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int h;
+    string pictrute;
+    cout << "Enter height 2-9: ";
+    cin >> h;
+    if (h <= 2 || h >= 9)
+        return 1;
+    for(int i = 0; i < h - 1; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+            for(int k = 0; k < 2 * h + 1; ++k)
+                if(h - i == k || h + i == k)
+                    pictrute += "*";
+                else pictrute += " ";
+        pictrute += "\n";
+    }
+    for(int i = 0; i < 3; ++i) 
+    {
+        pictrute += " ";
+        for(int j = 0; j < 2 * h - 1; ++j)
+            pictrute += "*";
+        pictrute += " ";
+    }
+    pictrute += "\n";
+    cout << pictrute;
+
+    return 0;
+
+}
