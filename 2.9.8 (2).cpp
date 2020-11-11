321118bf1c5cd4a8d62ff7938f95938338d83d08
Jenya0092 <56063125+Jenya0092@users.noreply.github.com>
2020-11-11 23:16:12 +0200

    Add files via upload

---
+++ 2.9.8 (2).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void) {
+    int money, calc;
+    cout<<"Enter your number: ";
+    cin>>money;
+    int banknots [5] = {50,20,10,5,1};  
+    for(int i = 0;i<5;i++){
+        while(money>=banknots[i]){
+            calc = money/banknots[i];
+            money=money%banknots[i];
+            for(int c = 1; c<=calc; c++){
+                cout<<banknots[i]<<" ";
+            }
+        }
+
+    }
+
+}
