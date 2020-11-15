dee726a9571743579c9d5a1cb1f07254e36be688
Jenya0092 <56063125+Jenya0092@users.noreply.github.com>
2020-11-15 19:04:01 +0200

    Add files via upload

---
+++ 2.9.8 (3).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main(void) {
+int vector1[] = {1,2,3,2,1};
+
+int n = sizeof(vector1) / sizeof(vector1[0]);
+
+n = n-1;
+int a = 0;
+
+for(int i =0; i<=n/2;i++){
+    int c = n-i;
+    if(vector1[i]==vector1[c]){
+a++;
+    if(a==(n+1)/2){cout<<"It is palindrom!";}
+    }
+    else{cout<<"it is not palindrom!";
+        break;
+    }
+
+}
+
+
+}
---
+++ 2.9.8 (4).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main(void) {
+double vector[] = { 1., 2., 3., 4., 5. };
+int n = sizeof(vector) / sizeof(vector[0]);
+double ArithmeticMean;
+double HarmonicMean;
+double GeometricMean=1;
+double RootMeanSquare;
+for(int i = 0;i<n; i++){
+    ArithmeticMean =ArithmeticMean + (vector[i]/n);
+    double c = c + (1/vector[i]);
+    HarmonicMean = n / c;
+    GeometricMean = GeometricMean*vector[i];
+    if(i==4){
+    GeometricMean =pow(GeometricMean,(1./n));
+    }
+    double r = r+pow(vector[i],2.);
+    RootMeanSquare = sqrt(r/n);
+    
+}
+
+
+cout << "Arithmetic Mean = " << ArithmeticMean << endl;
+cout << "Harmonic Mean = " << HarmonicMean << endl;
+cout << "Geometric Mean = " << GeometricMean << endl;
+cout << "RootMean Square = " << RootMeanSquare << endl;
+cout << endl;
+return 0;
+}
