8805c244f41ed81d252e74c125dcaf2a79a49137
Jenya0092 <56063125+Jenya0092@users.noreply.github.com>
2020-12-08 12:58:45 +0200

    Add files via upload

---
+++ 3.2.12(1).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int vector[] = { 3, -5, 7, 10, -4, -45, 5, 2, -13 };
+  int n = sizeof(vector) / sizeof(vector[0]);
+  int num = *(vector);
+  for(int i = 0; i<=n;i++){
+  if(*(vector+i)<num){
+      num = *(vector+i);
+  }
+  }
+  cout<<num;
+  return 0;
+}
---
+++ 3.4.7(1).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+bool isLeap(int year) {
+    bool yr;
+if(year%4 == 0 || year%100!=0 && year%400 ==0){
+    yr = true;
+}
+else {yr = false;}
+return yr;
+}
+int main(void) {
+for(int yr = 1995; yr < 2017; yr++)
+cout << yr << " -> " << isLeap(yr) << endl;
+return 0;
+}
